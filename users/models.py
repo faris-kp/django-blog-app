eb216c30569481ac372d9755250637a84561dc5e
faris-kp <fariskp46820@gmail.com>
2021-05-14 18:10:15 +0530

    create,list,detail view set

--- users/models.py
+++ users/models.py
@@ -13,10 +13,10 @@ class Profile(models.Model):
         return f'{ self.user.username }Profile'
 
 
-    def save(self):
-        super().save()
-
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
         img = Image.open(self.image.path)
+        img = img.rotate(angle=90,expand=True)
 
         if img.height > 300 or img.width > 300:
             output_size=(300, 300)
